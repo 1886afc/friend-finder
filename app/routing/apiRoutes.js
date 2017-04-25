7a03343425067b2ac290a49ba71c4a7d36030ffd
Israel Ponce <Israel Ponce>
2017-04-25 16:04:55 -0400

    working on html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,13 +13,55 @@ module.exports = function(app) {
 		res.json(friendsData);
 	});
 
-	// the code 
+	//  code for pushing new friend to friendsData array and determine best match 
 	app.post("/api/friends", function(req, res) {
 		var newFriend = req.body;
 		console.log(newFriend);
-		friendsData.push(newFriend);
-
+		
 		res.json(newFriend);
+		// holds best match
 
+		var bestMatch = {
+			name: "",
+			photo: "",
+			friendDiff: 1000
+		};
+
+		// users survey results
+		var userName = newFriend.name;
+		var photo = newFriend.photo;
+		var score = newFriend.scores;
+
+		// calculating the diff of users scores and those in the database
+		var totalDiff = 0;
+
+		//loop for all the users in the array
+		for (var i = 0; i < friendsData.length; i++) {
+			// console.log(friendsData[i].name);
+			// console.log(friendsData[i].photo);
+			totalDiff = 0;
+
+			//looping through all the scores
+			for (var j = 0; j < friendsData[i].scores[j]; j++) {
+				//calculating totalDiff between scores
+				totalDiff += Math.abs(
+					parseInt(score[j])-
+					parseInt(friendsData[i].scores[j]));
+
+				if (totalDiff <= bestMatch.friendDiff) {
+
+					bestMatch.name = friendsData[i].name;
+					bestMatch.photo = friendsData[i].photo;
+					bestMatch.friendDiff = totalDiff;
+					console.log("best match   " + JSON.stringify(bestMatch));
+				}
+
+				
+			}
+		}
+		//puches new user to database
+		friendsData.push(newFriend);
+		//returns json of best match to be used for html
+		res.json(bestMatch);
 	});
 };
